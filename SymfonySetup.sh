9caf0a70f1d9caf4eba2dd2f27328c2060039d41
D3vcode88 <D3vcode@protonmail.com>
2025-02-12 20:51:30 +0000

    ajout validation nom projet avec boucle & ajout menu iteratif avec boucle

--- SymfonySetup.sh
+++ SymfonySetup.sh
@@ -3,8 +3,32 @@
 
 echo "Bienvenue sur SymfonySetup"
 
-echo "Comment va s'appeler votre projet ?"
+menu="Quel op√erations voulez-vous effectue ?"
+options=("Verification des modules installer"
+	"Installer et configurer un projet avec ou sans docker"
+	"Quitter")
 
-read nameProject
+select option in "${options[@]}";
+do 
+	case $REPLY in
+	1)
+		echo "Module installer :"
+		;;
+	2)
+		echo "Veuilliez nomme votre projet :"
+		while [ -z $nameProject ]
+		do
+			echo "Comment va s'appeler votre projet ?"
+			read nameProject
+		done
 
-echo "Votre projet s'appel : $nameProject"
+		echo "Votre projet s'appel : $nameProject"
+		;;
+	3)
+		echo "Au revoir !"
+		break
+		;;
+	*)
+		echo "Saisi invalide, veuilliez reessayer !"
+	esac
+done
